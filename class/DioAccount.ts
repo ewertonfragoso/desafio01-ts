2de332b9308a72d30964df8cb335be6a8d8a1e7c
Ewerton Fragoso <ewertonfragoso@gmail.com>
2024-04-29 23:19:38 -0300

    feat: all attributes to private and name to readonly

--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,7 +1,7 @@
 export abstract class DioAccount {
-  private name: string
+  private readonly name: string
   private readonly accountNumber: number
-  balance: number = 0
+  private balance: number = 0
   private status: boolean = true
 
   constructor(name: string, accountNumber: number){
